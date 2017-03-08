1fbdeb2393e3fb0dfeb266053957a8a563d38ca2
Claudiu Lodromanean <claudiu@Claudius-MacBook-Pro.local>
2017-03-07 18:07:29 -0800

    SEO tweaks

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 	<div class="m-footer">
 		<?php wp_nav_menu( array( 'theme_location' => 'cn-footer-menu' ) ); ?>
+		<div class="copyright">Copyright <?php echo date( 'Y' ) ?> - <?php bloginfo( 'name' ) ?></div>
 	</div>
 </div>
 <?php wp_footer(); ?>
